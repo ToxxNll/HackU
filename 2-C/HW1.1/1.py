6c972592c8e1e44341f1ddbc9840964d271eecb2
ToxxNll <dolayevtohtar@gmail.com>
2022-01-18 23:13:04 +0600

    Homework#1

--- 2-C/HW1.1/1.py
+++ 2-C/HW1.1/1.py
@@ -6,7 +6,7 @@ class human():
         print("This is construct")
     
     def olding(self):
-        if type(self) == type(baby):
+        if type(self) != type(human):
             self.age -= 1
         else:
             self.age += 1
@@ -25,26 +25,3 @@ Dias = human("Dias", "Male", 23)
 obj = baby("qwe", "qwe", 9)
 obj.olding()
 print(obj.age)
-
-
-# class human():
-#     def __init__(self, iName, iPol, iAge):
-#         self.name = iName
-#         self.pol = iPol
-#         self.age = iAge
-#         print("This is construct")
-    
-#     def olding(self):
-#         if type(self) == :
-#             self.age -= 1
-#         self.age += 1
-        
-# class baby(human):    
-#     def olding(self):
-#         human.olding(self)
-        
-
-# Dias = human("Dias", "Male", 23)
-# obj = baby("qwe", "qwe", 9)
-# obj.olding()
-# print(type(obj))
---
+++ 2-C/HW1.1/2.py
@@ -0,0 +1,42 @@
+class human():
+    def __init__(self, iName, iPol, iAge):
+        self.name = iName
+        self.pol = iPol
+        self.age = iAge
+        print("This is construct")
+    
+    def olding(self):
+        if type(self) != type(human):
+            self.age -= 1
+        else:
+            self.age += 1
+        
+class baby(human):
+    def __init__(self, iName, iPol, iAge,iToy,ICan_Walk):
+        human.__init__(self,iName, iPol, iAge)
+        self.toy = iToy
+        self.can_walk = ICan_Walk
+        print("This baby construct")
+        
+    def speak(self):
+        if self.age >= 3:
+            self.speaking = True
+        else:
+            self.speaking = False
+
+    def olding(self):
+        human.olding(self)
+
+    def showinfo(self):
+        
+        try:
+            return self.name, self.pol, self.age, self.toy, self.can_walk,self.speaking
+        except:
+            return self.name, self.pol, self.age, self.toy, self.can_walk
+                
+
+Dias = human("Dias", "Male", 23)
+obj = baby("Tox", "Male", 9,'Buzzlighter',True)
+obj.olding()
+obj.speak()
+print(obj.showinfo())
